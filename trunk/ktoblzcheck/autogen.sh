564c6b7ff33221968e2d82ddea1df41af42bdcc1
cstim <cstim@6d9220a4-9c13-0410-b7b4-e94da3059260>
2005-10-21 11:45:46 +0000

    More cleanups

--- trunk/ktoblzcheck/autogen.sh
+++ trunk/ktoblzcheck/autogen.sh
@@ -1,18 +1,23 @@
 #!/bin/sh -rx
 
-# Make sure this script exits if a command was not found
-trap exit ERR
+# First cache the command names in variables. If you want to
+# override the names, simply set the variables before calling this
+# script.
 
+#: ${INTLTOOLIZE=intltoolize}
+#: ${GETTEXTIZE=gettextize}
 : ${LIBTOOLIZE=libtoolize}
 : ${ACLOCAL=aclocal}
 : ${AUTOMAKE=automake}
-: ${AUTOHEADER=autoheader}
 : ${AUTOCONF=autoconf}
+: ${AUTOHEADER=autoheader}
 
-${LIBTOOLIZE} -f --automake
-${ACLOCAL} -I macros ${ACLOCAL_FLAGS}
-${AUTOMAKE} --add-missing
-${AUTOHEADER}
-${AUTOCONF}
+#${INTLTOOLIZE}                          || exit 1
+#${GETTEXTIZE}                           || exit 1
+${LIBTOOLIZE} -f --automake             || exit 1
+${ACLOCAL} -I macros ${ACLOCAL_FLAGS}   || exit 1
+${AUTOMAKE} --add-missing               || exit 1
+${AUTOCONF}                             || exit 1
+${AUTOHEADER}                           || exit 1
 
 echo "Now you can run ./configure"
